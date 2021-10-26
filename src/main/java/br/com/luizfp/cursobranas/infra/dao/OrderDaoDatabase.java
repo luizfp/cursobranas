390df861dc83a15e1e9fbc6f1babba786fcf991e
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-10-26 12:47:04 -0300

    fix: corrige OrderDaoDatabase

--- src/main/java/br/com/luizfp/cursobranas/infra/dao/OrderDaoDatabase.java
+++ src/main/java/br/com/luizfp/cursobranas/infra/dao/OrderDaoDatabase.java
@@ -55,7 +55,7 @@ public final class OrderDaoDatabase implements OrderDao {
                                                       @NotNull final Long orderId) {
         final List<DatabaseResultRow> dbItems = tx.query("""
                                                                  select * from order_item oi
-                                                                 join stock_item si on oi.id_item = si.id
+                                                                 join stock_item si on oi.item_id = si.id
                                                                  where oi.order_id = ?
                                                                  """, orderId);
         final List<GetOrderItemOutput> items = new ArrayList<>();
